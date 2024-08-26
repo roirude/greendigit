19c1ce9d10e41bdef32d686c6d14368bbee56745
Rudex Tede <rudexjuniortede@gmail.com>
2024-08-26 23:31:11 +0100

    Add a view to display the list of farmers

--- greendigit/urls.py
+++ greendigit/urls.py
@@ -3,12 +3,13 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-from greendigit.views import index, StoreView
+from greendigit.views import index, StoreView, FarmerStoreView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
     path('store/', StoreView.as_view(), name='store'),
+    path('farmers/', FarmerStoreView.as_view(), name='farmers'),
     path('accounts/', include('allauth.urls')),
     path('user/', include('users.urls')),
     path('products/', include('products.urls')),
--- greendigit/views.py
+++ greendigit/views.py
@@ -1,8 +1,12 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render, redirect
 from django.views.generic import ListView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from users.mixins import GroupRequiredMixin
 from products.models import Product
+from users.models import CustomUser
  
 def index(request):
     if request.user.is_authenticated:
@@ -22,4 +26,14 @@ class StoreView(ListView):
     def get_queryset(self):
         products = Product.objects.filter(is_delete=False).order_by('-created_at')
         return products
+    
+
+class FarmerStoreView(LoginRequiredMixin, ListView):
+    model = CustomUser
+    template_name = 'greendigit/farmers.html'
+    paginate_by = 60
+    
+    def get_queryset(self):
+        farmers = CustomUser.objects.filter(is_farmer=True)
+        return farmers
     
