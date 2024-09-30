48fb286a96422f9a61df068544c402ca6c61756e
Rudex Tede <rudexjuniortede@gmail.com>
2024-09-30 15:03:57 +0100

    change receipt id generator system

--- payment/utils.py
+++ payment/utils.py
@@ -6,7 +6,7 @@ def generate_receipt_id(Receipt):
     ).order_by('id').last()
     
     if not latest_transaction:
-        return f"REC-{datetime.now().year}-0001"
+        return f"REC-{datetime.now().year}{datetime.now().month}{datetime.now().day}{datetime.now().hour}{datetime.now().minute}-0001"
     
     receipt_id = int(latest_transaction.receipt_id.split('-')[-1]) + 1
-    return f"REC-{datetime.now().year}-{receipt_id:04d}"
+    return f"REC-{datetime.now().year}{datetime.now().month}{datetime.now().day}{datetime.now().hour}{datetime.now().minute}-{receipt_id:04d}"
