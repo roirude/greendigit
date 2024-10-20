10378daa97b641cdbeb72b278b57d153feb09a30
Rudex Tede <rudexjuniortede@gmail.com>
2024-10-20 03:07:11 +0100

    Add security in a settings. Mask Email user and password

--- greendigit/settings.py
+++ greendigit/settings.py
@@ -196,12 +196,12 @@ ACCOUNT_SESSION_REMEMBER = None
 
 # Email configuration
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'rudextede1@gmail.com'
-EMAIL_HOST_PASSWORD = 'lpuotpcptkzcynwy'
-DEFAULT_FROM_EMAIL = 'rudextede1@gmail.com'
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_PORT = os.getenv('EMAIL_PORT')
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS')
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
 
 
 # Social Auth
